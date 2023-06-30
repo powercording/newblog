ec4aef2458ac6ba04bbfac47df4e87fae48df621
SangDon <adoim@naver.com>
2023-07-01 03:05:10 +0900

    Feat: 코드 하이라이팅 적용

--- src/app/addPost/page.tsx
+++ src/app/addPost/page.tsx
@@ -1,26 +1,42 @@
 "use client";
 
-"use client";
-
 import { useState } from "react";
 import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
-import rehypeSanitize from "rehype-sanitize";
 import rehypeRaw from "rehype-raw";
-import rehypePrism from "rehype-prism-plus";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { vscDarkPlus } from "react-syntax-highlighter/dist/esm/styles/prism";
 
 export default function AddPost() {
   const [markdown, setMarkdown] = useState("");
+
   return (
     <main className="grid lg:grid-cols-2 max-h-screen">
       <textarea
         onChange={(e) => setMarkdown(e.target.value)}
-        className="h-screen w-full"
+        className="h-screen w-full text-black p-10 lg:p-20"
       ></textarea>
       <ReactMarkdown
-        className="p-10 w-full prose"
+        className="p-10 lg:p-20 w-full prose"
+        components={{
+          code({ node, inline, className, children, ...props }) {
+            const match = /language-(\w+)/.exec(className || "");
+            return !inline && match ? (
+              <SyntaxHighlighter
+                {...props}
+                style={vscDarkPlus}
+                language={match[1] ?? "jsx"}
+                PreTag="div"
+              >
+                {String(children).replace(/\n$/, "")}
+              </SyntaxHighlighter>
+            ) : (
+              <code {...props}>{children}</code>
+            );
+          },
+        }}
         remarkPlugins={[remarkGfm]}
-        rehypePlugins={[rehypePrism, rehypeSanitize, rehypeRaw]}
+        rehypePlugins={[rehypeRaw]}
       >
         {markdown}
       </ReactMarkdown>
