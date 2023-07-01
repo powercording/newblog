818a4520a983d5f5dd8efacde060b1e24d3e3a10
powercording <adoim@naver.com>
2023-07-01 19:28:29 +0900

    Feat: 마크다운 컨텐츠 작성 페이지 구축 완료

--- src/app/addPost/page.tsx
+++ src/app/addPost/page.tsx
@@ -9,12 +9,18 @@ import { vscDarkPlus } from "react-syntax-highlighter/dist/esm/styles/prism";
 
 export default function AddPost() {
   const [markdown, setMarkdown] = useState("");
+  const [title, setTitle] = useState("");
+
+  const handlePostRegister = () => {
+    console.log(markdown);
+    console.log(title);
+  };
 
   return (
     <main className="grid lg:grid-cols-2 justify-items-stretch max-h-screen">
       <textarea
         onChange={(e) => setMarkdown(e.target.value)}
-        className="editor h-screen w-full text-black p-20"
+        className="editor h-screen w-full text-black p-20 focus:outline-none"
         placeholder="Write your markdown here..."
       ></textarea>
       <section className="p-20 prose max-h-screen">
@@ -44,6 +50,22 @@ export default function AddPost() {
           {markdown}
         </ReactMarkdown>
       </section>
+      <div className="flex border fixed h-16 bottom-0 inset-x-0 lg:inset-x-[300px] bg-white rounded-t-lg drop-shadow-md shadow-md">
+        <input
+          type="text"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          placeholder="제목을 입력해 주세요"
+          className="w-full h-full px-4 font-bold focus:outline-none bg-gray-50"
+        />
+        <button
+          onClick={handlePostRegister}
+          className="w-24 border-r hover:bg-zinc-200"
+        >
+          글쓰기
+        </button>
+        <button className="w-24 hover:bg-zinc-200">임시1</button>
+      </div>
     </main>
   );
 }
